e12c6e8f3b3a709cfa4d3e163373b34b616b4c7a
Stephen Gladney <stephengladney@gmail.com>
2023-02-23 20:36:17 -0500

    make header littles red

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,11 @@ module.exports = {
         fadein: "fade-in 0.25s ease-in",
         "fadein-1s": "fade-in 1s ease",
       },
-      colors: { disabled: "text-neutral-500", valero: "#2a6f8a" },
+      colors: {
+        disabled: "text-neutral-500",
+        valero: "#2a6f8a",
+        littles: "#b5121b",
+      },
       keyframes: {
         "fade-in": { "0%": { opacity: "0" }, "100%": { opacity: "1" } },
       },
