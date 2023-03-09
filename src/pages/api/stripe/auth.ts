267e5819673b5e936f8fda2f998f3f1da0b7a390
Stephen Gladney <stephengladney@gmail.com>
2023-03-09 18:32:56 -0500

    pass in origin for stripe callback uri

--- src/pages/api/stripe/auth.ts
+++ src/pages/api/stripe/auth.ts
@@ -2,6 +2,7 @@ import { env } from "../../../env/server.mjs"
 import type { NextApiRequest, NextApiResponse } from "next"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.headers)
   if (req.method === "POST") {
     try {
       res.redirect(
@@ -9,9 +10,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${
           env.STRIPE_CLIENT_ID
           // State includes Store ID so the callback updates the stripe ID in the database
-        }&scope=read_write&redirect_uri=http://localhost:3000/api/stripe/callback&state=${
-          req.query.id as string
-        }`
+        }&scope=read_write&redirect_uri=${req.headers
+          .origin!}/api/stripe/callback&state=${req.query.id as string}`
       )
     } catch ({ message }) {
       res.status(500).json(message as string)
