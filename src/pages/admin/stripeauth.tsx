0527c6ede7144b32f5a506e790762bdea6561612
Stephen Gladney <stephengladney@gmail.com>
2023-03-09 18:14:33 -0500

    prettify (sort of) stripe auth page

--- src/pages/admin/stripeauth.tsx
+++ src/pages/admin/stripeauth.tsx
@@ -2,11 +2,16 @@ import { type NextPage } from "next"
 
 const StripAuth: NextPage = () => {
   return (
-    <>
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <h1 className="mb-12 font-poppins text-2xl font-bold">
+        Click here to connect Stripe
+      </h1>
       <form action="/api/stripe/auth?id=1" method="POST">
-        <button>Authorize Stripe</button>
+        <button className="w-72 rounded-full bg-indigo-800 p-4 font-poppins font-bold text-white">
+          Authorize Stripe
+        </button>
       </form>
-    </>
+    </div>
   )
 }
 
