15775d26f1bc2a4be5d7782e0774528322182d91
Stephen Gladney <stephengladney@gmail.com>
2023-04-01 10:48:44 -0400

    Show user and stores while connecting Stripe

--- src/pages/admin/stripeauth.tsx
+++ src/pages/admin/stripeauth.tsx
@@ -1,6 +1,16 @@
 import { type NextPage } from "next"
+import { useSession } from "next-auth/react"
+import { api } from "../../utils/api"
 
 const StripeAuth: NextPage = () => {
+  const { data: session } = useSession({ required: true })
+  const { data: user } = api.user.getByEmail.useQuery(
+    { email: session?.user.email as string },
+    { enabled: !!session }
+  )
+  const { data: stores } = api.store.getByIds.useQuery({
+    ids: user?.stores ?? [],
+  })
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center">
       <h1 className="mb-12 font-poppins text-2xl font-bold">
@@ -11,6 +21,21 @@ const StripeAuth: NextPage = () => {
           Authorize Stripe
         </button>
       </form>
+      <div className="mt-10">
+        <h3>{user?.email}</h3>
+        <h3>
+          <select
+            className="p-4 focus:outline-none"
+            onChange={(e) => console.log(e.target.value)}
+          >
+            {stores?.map((store, i) => (
+              <option key={i} value={store.id}>
+                {store.name}
+              </option>
+            ))}
+          </select>
+        </h3>
+      </div>
     </div>
   )
 }
