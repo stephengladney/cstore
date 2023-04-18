2a898cb8c9b170680a050313a352104d322acaf8
Stephen Gladney <stephengladney@gmail.com>
2023-04-18 16:20:08 -0400

    add phone number. adjust line height

--- src/components/OrderingContainer/OrderingContainer.tsx
+++ src/components/OrderingContainer/OrderingContainer.tsx
@@ -9,6 +9,7 @@ import type { CartItem } from "../../types/Cart"
 import { OrderCart } from "../OrderCart/OrderCart"
 import { dimmerContext } from "../../contexts/dimmerContext"
 import { storeContext } from "../../contexts/storeContext"
+import { formatPhoneNumber } from "../OrdersScreen/OrderDetailsComponent"
 
 export function OrderingContainer({}) {
   const [selectedItem, setSelectedItem] = useState<
@@ -60,7 +61,10 @@ export function OrderingContainer({}) {
             <br />
             {store.name}
           </h1>
-          <h2 className="text-sm">{store.address}</h2>
+          <h2 className="mt-1 text-sm leading-6">{store.address}</h2>
+          <h2 className="text-sm leading-5">
+            {formatPhoneNumber(store.phone as string)}
+          </h2>
         </div>
         <OrderingMenu setSelectedItem={setSelectedItem} />
         <OrderItemModal
