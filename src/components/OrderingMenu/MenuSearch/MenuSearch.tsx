139f7c41ca9a5d085af0f2e7768131ad07a10bd2
Stephen Gladney <stephengladney@gmail.com>
2023-04-18 14:29:09 -0400

    add clear button

--- src/components/OrderingMenu/MenuSearch/MenuSearch.tsx
+++ src/components/OrderingMenu/MenuSearch/MenuSearch.tsx
@@ -5,6 +5,7 @@ import {
   useTransition,
   useState,
 } from "react"
+import { IoMdClose } from "react-icons/io"
 
 export function MenuSearch({
   setSearchQuery,
@@ -14,19 +15,33 @@ export function MenuSearch({
   const [inputValue, setInputValue] = useState("")
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, startTransition] = useTransition()
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value)
     startTransition(() => {
       setSearchQuery(e.target.value)
     })
   }
+
+  const handleCancel = () => {
+    setInputValue("")
+    startTransition(() => {
+      setSearchQuery("")
+    })
+  }
   return (
-    <div className="mt-6 mb-4 px-4  md:mt-10  md:px-0">
+    <div className="mt-6 mb-4 flex  items-center  px-4 md:mt-10 md:px-0">
       <input
         className="border-gr w-full max-w-[500px] rounded-lg border-[1px] border-solid border-gray-300 py-2 px-4 font-poppins"
         onChange={handleChange}
         placeholder="Search..."
         value={inputValue}
+      />
+      <IoMdClose
+        size={20}
+        fill="#A1A1AA"
+        className="z-10 ml-[-35px] cursor-pointer"
+        onClick={handleCancel}
       />
     </div>
   )
