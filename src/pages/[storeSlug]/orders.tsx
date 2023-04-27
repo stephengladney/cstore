bbe9c44adf05e4c2375c7b03ed24705862b2d938
Stephen Gladney <stephengladney@gmail.com>
2023-04-27 17:17:21 -0400

    better loading state

--- src/pages/[storeSlug]/orders.tsx
+++ src/pages/[storeSlug]/orders.tsx
@@ -70,7 +70,12 @@ const Orders: NextPage<{ store: Store }> = ({ store }: { store: Store }) => {
 
   const handleBackClick = () => setSelectedOrder(null)
 
-  if (isLoading || status === "loading") return <div>Loading...</div>
+  if (isLoading || status === "loading")
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <h1 className="text-6xl">Loading app...</h1>
+      </div>
+    )
   if (!isLoading && !isUserHasAccess) return <div>Not Allowed</div>
 
   return (
